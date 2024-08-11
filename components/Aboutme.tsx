b81674b09860ad7a1a51851e374eb800ee6ae4ea
TaherBabuji <121304971+TaherBabuji@users.noreply.github.com>
2024-08-11 18:01:37 +0530

    Update Aboutme.tsx

--- components/Aboutme.tsx
+++ components/Aboutme.tsx
@@ -10,12 +10,12 @@ const Aboutme = () => {
           As a JavaScript backend developer with hands-on experience from a
           four-month internship and completed projects, I offer comprehensive
           services in web development with a focus on backend technologies.
-          Here's what makes me stand out: Practical Experience: During my
+          Here&#39;s what makes me stand out: Practical Experience: During my
           internship, I actively contributed to projects using JavaScript,
           Express, Node.js, and MongoDB. This hands-on experience has honed my
           skills in building scalable and efficient backend systems. Project
           Portfolio: I've completed projects that showcase my ability to develop
-          robust backend solutions. For example, I've built RESTful APIs,
+          robust backend solutions. For example, I&#39;ve built RESTful APIs,
           implemented authentication and authorization mechanisms, and
           integrated databases to store and retrieve data efficiently. my
           combination of practical experience, project portfolio,
