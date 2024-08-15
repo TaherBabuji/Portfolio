a41344ef881e6424a3b99fea60ca878ba784a5d6
TaherBabuji <121304971+TaherBabuji@users.noreply.github.com>
2024-08-16 02:25:46 +0530

    Update ProjectCard.tsx

--- components/ProjectCard.tsx
+++ components/ProjectCard.tsx
@@ -2,25 +2,33 @@ import Image from "next/image";
 interface Props {
   title: string;
   link: string;
+  link_demo: string;
   img: string;
 }
 
-const ProjectCard = ({ title, link, img }: Props) => {
+const ProjectCard = ({ title, link, img, link_demo }: Props) => {
   return (
-    <a href={link} target="_blank">
-      <div className="w-full h-80 border-[1px] border-blue-600 overflow-hidden relative rounded-lg group">
-        <Image
-          className="object-cover translate-y-0 group-hover:-translate-y-[65%] transition-transform duration-[3s]"
-          width={350}
-          height={350}
-          src={img}
-          alt="Amazonclone"
-        />
-        <p className="absolute hidden group-hover:inline-block bottom-0 w-full py-1 bg-blue-600 text-white text-center font-semibold duration-300">
-          {title}
-        </p>
-      </div>
-    </a>
+    <div className="flex flex-col items-center">
+      <a href={link} target="_blank">
+        <div className="w-full h-80 border-[1px] border-blue-600 overflow-hidden relative rounded-lg group">
+          <Image
+            className="object-cover translate-y-0 group-hover:-translate-y-[65%] transition-transform duration-[3s]"
+            width={350}
+            height={350}
+            src={img}
+            alt="Amazonclone"
+          />
+          <p className="absolute hidden group-hover:inline-block bottom-0 w-full py-1 bg-blue-600 text-white text-center font-semibold duration-300">
+            {title}
+          </p>
+        </div>
+      </a>
+      <a href={link} className="mt-4 hover:text-blue-500">
+        <h1 className="text-2xl  font-bold text-white hover:text-blue-500">
+          {link_demo}
+        </h1>
+      </a>
+    </div>
   );
 };
 
