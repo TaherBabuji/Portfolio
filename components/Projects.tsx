3995326ec5ecc917dddcfd5d69aac4e2e631cde7
TaherBabuji <121304971+TaherBabuji@users.noreply.github.com>
2024-08-16 02:26:28 +0530

    Update Projects.tsx

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -3,6 +3,7 @@ import Title from "./Title";
 import clean from "../public/img/projects/clean.png";
 import videoTube from "../public/img/projects/videoTube.png";
 import ProjectCard from "./ProjectCard";
+import moviereview from "../public/img/projects/moviereview.jpeg";
 
 const Projects = () => {
   return (
@@ -12,12 +13,20 @@ const Projects = () => {
         <ProjectCard
           img={clean.src}
           title="Clean Blog"
-          link="https://blogpost-project-9pv1.onrender.com/"
+          link="https://github.com/TaherBabuji/blogpost-project"
+          link_demo="Live Demo"
+        />
+        <ProjectCard
+          img={moviereview.src}
+          title="ReelReview"
+          link="https://github.com/TaherBabuji/blogpost-project"
+          link_demo="Live Demo"
         />
         <ProjectCard
           img={videoTube.src}
           title="VideoTube"
           link="https://github.com/TaherBabuji/VideoTube_Backend-Youtube-clone-"
+          link_demo="Github Link"
         />
       </div>
     </div>
